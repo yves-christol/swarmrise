4b62574f30a64ac74718f89cbec77f70de840fd9
Yves Christol <yves.christol@gmail.com>
2025-11-01 15:04:37 +0100

    A few fixes

--- src/App.tsx
+++ src/App.tsx
@@ -12,9 +12,9 @@ import { Anonymous } from "./components/Anonymous"
 export default function App() {
   return (
     <>
-      <header className="sticky top-0 z-10 bg-light dark:bg-dark p-4 border-b-2 border-slate-200 dark:border-slate-800 flex flex-row justify-between items-center">
+      <header className="sticky top-0 z-10 bg-dark p-4 border-b-2 border-slate-200 dark:border-slate-800 flex flex-row justify-between items-center">
         <Logo size={24} begin={2} repeatCount={1} /><b>swarmrise</b>
-        <UserButton />
+        <UserButton appearance={{ baseTheme: "dark" }} />
       </header>
       <main className="p-8 flex flex-col gap-16">
         <Authenticated>

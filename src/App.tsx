fee187f9b15974c17aec07dcb210c581ad44c7d3
Yves Christol <yves.christol@gmail.com>
2025-11-02 08:20:53 +0100

    fixed font

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { dark } from '@clerk/themes'
 
 import {
   Authenticated,
@@ -14,7 +15,7 @@ export default function App() {
     <>
       <header className="sticky top-0 z-10 bg-dark p-4 border-b-2 border-slate-200 dark:border-slate-800 flex flex-row justify-between items-center">
         <Logo size={24} begin={2} repeatCount={1} /><b>swarmrise</b>
-        <UserButton appearance={{ baseTheme: "dark" }} />
+        <UserButton appearance={{ theme: dark }} />
       </header>
       <main className="p-8 flex flex-col gap-16">
         <Authenticated>

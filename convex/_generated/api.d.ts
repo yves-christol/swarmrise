b5f183e55146de532f8b9507045fc86675bfd580
Yves Christol <yves.christol@gmail.com>
2025-11-03 09:43:37 +0100

    fixed font

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -8,13 +8,12 @@
  * @module
  */
 
-import type * as myFunctions from "../myFunctions.js";
-
 import type {
   ApiFromModules,
   FilterApi,
   FunctionReference,
 } from "convex/server";
+import type * as myFunctions from "../myFunctions.js";
 
 /**
  * A utility for referencing Convex functions in your app's API.
@@ -27,15 +26,11 @@ import type {
 declare const fullApi: ApiFromModules<{
   myFunctions: typeof myFunctions;
 }>;
-declare const fullApiWithMounts: typeof fullApi;
-
 export declare const api: FilterApi<
-  typeof fullApiWithMounts,
+  typeof fullApi,
   FunctionReference<any, "public">
 >;
 export declare const internal: FilterApi<
-  typeof fullApiWithMounts,
+  typeof fullApi,
   FunctionReference<any, "internal">
 >;
-
-export declare const components: {};
